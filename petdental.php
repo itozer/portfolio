f006ab06b538c9eaabe7a256784485c16a23cfea
isaac <itozer@gmail.com>
2017-01-29 22:09:28 -0800

    minor updates

--- petdental.php
+++ petdental.php
@@ -9,5 +9,14 @@
         $page = "PetDental";
         include("include/secondary_navigation.php");
     ?>
+
+    <section id="tz-secondary-header">
+        <h3>PetDental</h3>
+        <p>
+            PetDental is an iPhone app I built colloborating with a veterinarian friend of mine. PetDental enables users to take photos of their dogs or cats teeth and gums, and send them to a registered veterinarian for professional advice on ways to improve their pets health.
+        </p>
+    </section>
+
+
 </body>
 </html>
--- skydump.php
+++ skydump.php
@@ -13,7 +13,7 @@
     <section id="tz-secondary-header">
         <h3>SkyDump</h3>
         <p>
-            I created SkyDump to save the office, and myself, a bit of time and frustration when nature calls. SkyDump tells you what bathrooms are available so that you don't have to walk around looking for one that is free. You can check out a demo of the app <a href="http://www.skydump.info" target="_blank">here</a>, or below.
+            I created SkyDump to save the office, and myself, a bit of time and frustration when nature calls. SkyDump tells you what bathrooms are available so that you don't have to walk around looking for one that is free. You can check out a demo of the app <a href="http://skydump.info" target="_blank">here</a>, or below.
         </p>
     </section>
 
@@ -22,9 +22,9 @@
 
             <div class="tz-project-screen">
                 <div id="tz-skydump-wrapper" class="tz-project-screen-wrapper">
-                    <iframe src="http://www.skydump.info"></iframe>
+                    <iframe src="http://skydump.info"></iframe>
                 </div>
-            </div>2
+            </div>
 
 
             <p>SkyDump uses a Raspberry Pi and magnetic sensors to detect when a bathrooms state changes, and then updates the website to let everyone know.</p>

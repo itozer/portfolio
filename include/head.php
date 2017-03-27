561677c4d6ad63daf26c5df4b1f5d4a90d519027
itozer <itozer@gmail.com>
2017-03-27 00:01:13 -0700

    updates to look and feel

--- include/head.php
+++ include/head.php
@@ -2,13 +2,15 @@
     <meta name="description" content="Hi! My name is Isaac Tozer. I'm a full stack web developer and interface designer. Take a look at some of my recent projects.">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!--
     <script src="js/app.js" type="text/javascript"></script>
     <link rel="stylesheet" type="text/css" href="css/main.css">
     <link rel="stylesheet" type="text/css" href="css/directionalHover.css">
     <link rel="stylesheet" type="text/css" href="css/responsive.css">
-    -->
+    <!--
     <script src="dist/scripts.min.js" type="text/javascript" async></script>
     <link rel="stylesheet" type="text/css" href="dist/styles.min.css">
+    -->
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400|Open+Sans:300,400,600" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400" rel="stylesheet"> 
     <link rel="shortcut icon" href="images/favicon.ico" />
